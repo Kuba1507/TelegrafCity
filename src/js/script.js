d3138a7625c9b81b652ef60a98b80ca6aeaaa353
Kuba1507 <kuba.romanski@onet.com.pl>
2024-01-04 11:26:44 +0100

    styled navbar for tablets

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const burgerBtn = document.querySelector(".navbar__hamburger");
+const navigation = document.querySelector(".navbar");
 const navbar = document.querySelector(".navbar__menu");
 const navbarList = document.querySelector(".navbar__menu-list");
 const navLinks = document.querySelectorAll(".navbar__menu-list-item");
@@ -13,7 +14,13 @@ const closeNav = () => {
 	navbarList.classList.remove("show-list");
 };
 
+const handleShadow = () => {
+	navigation.classList.toggle("show-shadow");
+	navbar.classList.toggle("show-shadow");
+};
+
 navLinks.forEach((link) => {
 	link.addEventListener("click", closeNav);
 });
 burgerBtn.addEventListener("click", handleNav);
+burgerBtn.addEventListener("click", handleShadow);
